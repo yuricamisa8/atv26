926affe0a8fa5e6c8c301b5370b97a8e69cded56
yuricamisa8 <francisco.cardoso94@aluno.ce.gov.br>
2024-09-25 16:03:25 -0300

    first commit

---
+++ atv.py
@@ -0,0 +1,21 @@
+# Crie um programa que receba as notas de 5 alunos e as armazene em uma lista. O programa deve exibir a maior nota, a menor nota e a média das notas.
+
+# Exemplo de entrada:
+# Nota do aluno 1: 8
+# Nota do aluno 2: 6
+# Nota do aluno 3: 9
+# Nota do aluno 4: 7
+# Nota do aluno 5: 5
+
+# Exemplo de saída:
+# Maior nota: 9
+# Menor nota: 5
+# Média das notas: 7.0
+
+maior = []
+for n in range(5):
+    n = int(input("digite uma nota"))
+    maior.append(n)
+print(max(maior))
+print(min(maior))
+print(sum(maior) / len(maior))
